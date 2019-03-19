71bdeae7af82d1bb2015fa4898141c272c70ef62
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2019-03-19 16:07:38 +0100

    Use precision as a stopping criterion.

--- src/main/java/de/tud/ke/rulelearning/learner/StoppingCriterionLearner.java
+++ src/main/java/de/tud/ke/rulelearning/learner/StoppingCriterionLearner.java
@@ -1,6 +1,9 @@
 package de.tud.ke.rulelearning.learner;
 
 import de.tud.ke.rulelearning.experiments.StoppingCriterionConfiguration;
+import de.tud.ke.rulelearning.heuristics.ConfusionMatrix;
+import de.tud.ke.rulelearning.heuristics.Heuristic;
+import de.tud.ke.rulelearning.heuristics.Precision;
 import de.tud.ke.rulelearning.learner.prediction.Predictor;
 import de.tud.ke.rulelearning.learner.prediction.RuleSetPredictor;
 import de.tud.ke.rulelearning.model.*;
@@ -11,6 +14,9 @@ import weka.core.TechnicalInformation;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class StoppingCriterionLearner extends AbstractMultiLabelRuleLearner<StoppingCriterionConfiguration> {
 
@@ -45,11 +51,26 @@ public class StoppingCriterionLearner extends AbstractMultiLabelRuleLearner<Stop
         double threshold = getConfiguration().getStoppingCriterionThreshold();
 
         if (threshold < 1) {
-            int numRules = (int) Math.round(finalizedModel.size() * threshold);
+            List<Rule> rules = new ArrayList<>(finalizedModel);
+            final Heuristic precision = new Precision();
+            rules.sort(((Comparator<Rule>) (rule1, rule2) -> {
+                Condition condition1 = rule1.getHead().getConditions().iterator().next();
+                Condition condition2 = rule2.getHead().getConditions().iterator().next();
+                ConfusionMatrix confusionMatrix1 = rule1.getHead().getLabelWiseConfusionMatrix(condition1.index());
+                ConfusionMatrix confusionMatrix2 = rule2.getHead().getLabelWiseConfusionMatrix(condition2.index());
+                double h1 = precision.evaluateConfusionMatrix(confusionMatrix1);
+                double h2 = precision.evaluateConfusionMatrix(confusionMatrix2);
+                return Double.compare(h1, h2);
+            }).reversed());
 
-            for (int i = finalizedModel.size() - 1; i >= numRules; i--) {
-                finalizedModel.remove(i);
+
+            int numRules = (int) Math.round(rules.size() * threshold);
+
+            for (int i = rules.size() - 1; i >= numRules; i--) {
+                rules.remove(i);
             }
+
+            return new RuleList(rules);
         }
 
         return finalizedModel;
---
+++ src/main/java/de/tud/ke/rulelearning/model/RuleList.java
@@ -0,0 +1,180 @@
+package de.tud.ke.rulelearning.model;
+
+import de.mrapp.util.datastructure.SortedArrayList;
+import org.jetbrains.annotations.NotNull;
+import weka.core.Instance;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RuleList implements RuleCollection, List<Rule>, RandomAccess {
+
+    private final List<Rule> list;
+
+    public RuleList() {
+        this.list = new ArrayList<>();
+    }
+
+    public RuleList(final List<Rule> list) {
+        this.list = list;
+    }
+
+    @Override
+    public RuleCollection getCoveringRules(Instance instance) {
+        return list.stream().filter(rule -> rule.covers(instance)).collect(Collectors.toCollection(RuleList::new));
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Rule> iterator() {
+        return list.iterator();
+    }
+
+    @NotNull
+    @Override
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    @NotNull
+    @Override
+    public <T> T[] toArray(@NotNull T[] ts) {
+        return list.toArray(ts);
+    }
+
+    @Override
+    public boolean add(Rule rule) {
+        return list.add(rule);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    @Override
+    public boolean containsAll(@NotNull Collection<?> collection) {
+        return list.containsAll(collection);
+    }
+
+    @Override
+    public boolean addAll(@NotNull Collection<? extends Rule> collection) {
+        return list.addAll(collection);
+    }
+
+    @Override
+    public boolean addAll(int i, @NotNull Collection<? extends Rule> collection) {
+        return list.addAll(i, collection);
+    }
+
+    @Override
+    public boolean removeAll(@NotNull Collection<?> collection) {
+        return list.removeAll(collection);
+    }
+
+    @Override
+    public boolean retainAll(@NotNull Collection<?> collection) {
+        return list.retainAll(collection);
+    }
+
+    @Override
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public Rule get(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public Rule set(int i, Rule rule) {
+        return list.set(i, rule);
+    }
+
+    @Override
+    public void add(int i, Rule rule) {
+        list.add(i, rule);
+    }
+
+    @Override
+    public Rule remove(int i) {
+        return list.remove(i);
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return list.indexOf(o);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return list.lastIndexOf(o);
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<Rule> listIterator() {
+        return list.listIterator();
+    }
+
+    @NotNull
+    @Override
+    public ListIterator<Rule> listIterator(int i) {
+        return list.listIterator(i);
+    }
+
+    @NotNull
+    @Override
+    public List<Rule> subList(int i, int i1) {
+        return list.subList(i, i1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        Iterator<Rule> iterator = iterator();
+
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next().toString());
+
+            if (iterator.hasNext()) {
+                stringBuilder.append(",\n");
+            }
+        }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(list);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RuleList rules1 = (RuleList) o;
+        return Objects.equals(list, rules1.list);
+    }
+
+
+}
