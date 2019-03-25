5c717bc48951f69501f4596539e83b9d713e2957
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2019-03-25 18:37:41 +0100

    Fix last commit.

--- src/main/java/de/tud/ke/rulelearning/learner/StoppingCriterionLearner.java
+++ src/main/java/de/tud/ke/rulelearning/learner/StoppingCriterionLearner.java
@@ -65,11 +65,16 @@ public class StoppingCriterionLearner extends AbstractMultiLabelRuleLearner<Stop
 
 
             int numRules = (int) Math.round(rules.size() * threshold);
+            Rule lastRule = rules.get(numRules - 1);
             double minPrecision = precision.evaluateConfusionMatrix(
-                    rules.get(numRules - 1).getConfusionMatrix());
+                    lastRule.getHead().getLabelWiseConfusionMatrix(
+                            lastRule.getHead().getConditions().iterator().next().index()));
 
             for (int i = rules.size() - 1; i >= numRules; i--) {
-                double h = precision.evaluateConfusionMatrix(rules.get(i).getConfusionMatrix());
+                Rule rule = rules.get(i);
+                Head head = rule.getHead();
+                double h = precision.evaluateConfusionMatrix(head.getLabelWiseConfusionMatrix(
+                        head.getConditions().iterator().next().index()));
 
                 if (h < minPrecision) {
                     rules.remove(i);
