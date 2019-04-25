93e1f2d89e6b27ab01ddd269295d331e0a3673a4
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2019-04-25 13:50:28 +0200

    Organize imports.

--- src/main/java/de/tud/ke/rulelearning/MainStoppingCriterionFMeasure.java
+++ src/main/java/de/tud/ke/rulelearning/MainStoppingCriterionFMeasure.java
@@ -1,19 +1,10 @@
 package de.tud.ke.rulelearning;
 
-import de.mrapp.util.IteratorUtil;
 import de.tud.ke.rulelearning.experiments.*;
-import de.tud.ke.rulelearning.heuristics.FMeasure;
 import de.tud.ke.rulelearning.heuristics.Heuristic;
-import de.tud.ke.rulelearning.learner.AbstractRuleGenerationLearner;
-import de.tud.ke.rulelearning.learner.RandomForestRuleGenerationLearner;
 import de.tud.ke.rulelearning.learner.StoppingCriterionLearner;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.function.BiFunction;
-import java.util.stream.IntStream;
 
 public class MainStoppingCriterionFMeasure {
 
