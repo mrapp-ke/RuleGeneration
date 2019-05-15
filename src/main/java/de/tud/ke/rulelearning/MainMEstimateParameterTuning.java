f07f729e9c4ccbab0a317dbc01759fe8e0f2da44
Michael Rapp <mrapp@ke.tu-darmstadt.de>
2019-05-15 18:13:24 +0200

    Add main class for parameter tuning.

---
+++ src/main/java/de/tud/ke/rulelearning/MainMEstimateParameterTuning.java
@@ -0,0 +1,44 @@
+package de.tud.ke.rulelearning;
+
+import de.tud.ke.rulelearning.experiments.*;
+import de.tud.ke.rulelearning.heuristics.Heuristic;
+import de.tud.ke.rulelearning.heuristics.MEstimate;
+import de.tud.ke.rulelearning.learner.AbstractRuleGenerationLearner;
+import de.tud.ke.rulelearning.learner.RandomForestRuleGenerationLearner;
+import de.tud.ke.rulelearning.learner.StoppingCriterionLearner;
+
+import java.util.function.BiFunction;
+
+public class MainMEstimateParameterTuning {
+
+    public static void main(String... args) {
+        BatchExperiment<BaseConfiguration> batchExperiment = new BatchExperiment<>(
+                (baseConfiguration, args1) -> baseConfiguration, args);
+        StoppingCriterionConfiguration.Builder configurationBuilder =
+                new StoppingCriterionConfigurationBuilderFactory<>(StoppingCriterionConfiguration.Builder::new)
+                        .create(batchExperiment.getConfiguration(), args);
+
+        final BiFunction<String, RuleGenerationConfiguration, AbstractRuleGenerationLearner> learnerFactory1 =
+                RandomForestRuleGenerationLearner::new;
+        final BiFunction<String, StoppingCriterionConfiguration, StoppingCriterionLearner> learnerFactory2 =
+                StoppingCriterionLearner::new;
+
+
+        for (Heuristic heuristic : new MainCoveringMEstimate.HeuristicIterable()) {
+            if (heuristic instanceof MEstimate && ((MEstimate) heuristic).getM() <= 4096) {
+                configurationBuilder.setCoveringHeuristic(heuristic);
+                batchExperiment.addExperiment(sharedData -> new RuleGenerationExperiment(sharedData,
+                        configurationBuilder.build(), learnerFactory1, ""));
+
+                for (double threshold : MainStoppingCriterionMEstimate.THRESHOLDS) {
+                    configurationBuilder.setStoppingCriterionThreshold(threshold);
+                    batchExperiment.addExperiment(sharedData -> new StoppingCriterionExperiment(sharedData,
+                            configurationBuilder.build(), learnerFactory2, ""));
+                }
+            }
+        }
+
+        batchExperiment.run();
+    }
+
+}
